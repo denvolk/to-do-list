e723722a0cec0347cce02bac3b6b7d621c7a2525
Denis Volkhin <denvolk242@gmail.com>
2022-05-27 17:31:37 +0300

    Add drag and drop Add drag and drop feature Fix line-through after drag and drop later

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,84 @@
 let images;
 let checkboxes;
 
+let dragEl = null;
+let items;
+
+//let completed = false;
+
+function dragStart(e)    {
+    this.classList.add('drag');
+
+    dragEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+
+    //if (this.classList[1] === 'completed')
+        //completed = true;
+    //console.log(this.classList[1]);
+    //console.log(completed);
+    //console.log(this.parentNode.innerHTML);
+    //console.log(this.innerHTML);
+    //console.log('dragStart');
+}
+function dragOver(e)    {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    //console.log('dragOver');
+    //return false;
+}
+function dragEnter()    {
+    this.classList.add('over');
+    //console.log('dragEnter');
+}
+function dragLeave(e)   {
+    e.stopPropagation();
+    this.classList.remove('over');
+    //console.log('dragLeave');
+}
+function dragDrop(e)    {
+    e.stopPropagation();
+
+    if (dragEl !== this)    {
+        let firstIsChecked = false;
+        let secondIsChecked = false;
+        if (this.querySelector('input[type=checkbox]').checked)
+            secondIsChecked = true;
+        if (dragEl.querySelector('input[type=checkbox]').checked)
+            firstIsChecked = true;
+        //console.log(this.innerHTML);
+        //console.log(dragEl.innerHTML);
+        dragEl.innerHTML = this.innerHTML;
+        this.innerHTML = e.dataTransfer.getData('text/html');
+        //console.log(this.innerHTML);
+        //console.log(dragEl.innerHTML);
+        if (firstIsChecked)
+            this.querySelector('input[type=checkbox]').checked = true;
+        if (secondIsChecked)
+            dragEl.querySelector('input[type=checkbox]').checked = true;
+
+        /*if (this.querySelector('input[type=checkbox]').checked)
+        if (completed)  {
+            this.querySelector('input[type=checkbox]').checked = true;
+            completed = false;
+        }*/
+        //this.getElementsByClassName('input[type=checkbox]')[0].innerHTML = e1.dataTransfer.getData('text/html');
+    }
+
+    //console.log('dragDrop');
+    return false;
+}
+function dragEnd() {
+    this.classList.remove('drag');
+
+    items.forEach(function (item)   {
+       item.classList.remove('over');
+    });
+
+    //console.log('dragEnd');
+}
+
 function addTask()  {
     let task = document.createElement('div');
     let checkbox = document.createElement('input');
@@ -40,6 +118,20 @@ function addTask()  {
     for (let iter = 0; iter < checkboxes.length; iter++)    {
         checkboxes[iter].addEventListener('change', setCompleted);
     }
+    /*add drag event----------------------------------------------------------*/
+    items = document.querySelectorAll('.task-list .task');
+
+    items.forEach(function(item) {
+        item.addEventListener('dragstart', dragStart);
+        item.addEventListener('dragend', dragEnd);
+
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+
+        item.addEventListener('dragover', dragOver);
+
+        item.addEventListener('drop', dragDrop);
+    });
     /*delete task-------------------------------------------------------------*/
     images = document.querySelectorAll('.images');
 
